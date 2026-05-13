90988972cbc9539f436476e01c49a0e360dc3694
devigMurilo <igormurilo.ac.21@gamil.com>
2026-05-12 22:27:30 -0300

    resolver o login

--- projeto/frontend/src/App.jsx
+++ projeto/frontend/src/App.jsx
@@ -1,27 +1,7 @@
 import { useEffect, useState } from 'react'
-
+import Login from './pages/login'
 function App() {
-  const [aluno, setAluno] = useState('Carregando...')
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/aluno/')
-      .then((res) => res.json())
-      .then((data) => {
-        setAluno(data)
-      })
-      .catch((error) => {
-        console.error(error)
-        setAluno('Erro ao conectar API')
-      })
-  }, [])
-
-  return (
-    <div>
-      <h1>{aluno.nome}</h1>
-      <p>Curso: {aluno.curso}</p>
-      <p>Idade: {aluno.idade}</p>
-    </div>
-  )
+    return <login />
 }
 
 export default App
--- projeto/frontend/src/pages/login.jsx
+++ projeto/frontend/src/pages/login.jsx
@@ -1,20 +1,40 @@
-const login = async () => {
+import { useState } from "react";
 
-  const response = await fetch(
-    "http://127.0.0.1:8000/api/login/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username,
-        password
-      })
-    }
-  )
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const data = await response.json()
+    const login = async () => {
+        const response = await fetch("http://localhost:8000/api/login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
 
-  console.log(data)
-}
+        const data = await response.json();
+
+        console.log(data);
+
+        localStorage.setItem("token", data.token);
+
+
+    };
+    return (
+        <div>
+            <h1>Login</h1>
+            <input
+                placeholder="matricula"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+            type="password"
+            placeholder="senha"
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={login}>Login</button>
+        </div>
+    );
+            }
